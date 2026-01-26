c4faffeeaf9187007aceb18ad1dd676d7ff9e5fd
PREM <prem92006@gmail.com>
2026-01-26 20:40:46 +0530

    Create page.tsx

---
+++ wedding-photography-website/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Wedding Photography Website</h1>
+      <p>Successfully deployed with v0 + Next.js 🚀</p>
+    </main>
+  );
+}
